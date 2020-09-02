66e595fe650ca963e935bdb6c6473c9f75ba0553
Hoang <hoangnguyen1247@gmail.com>
2020-09-02 15:15:02 +0700

    correct function parse key

--- src/utils/parse-filekey.ts
+++ src/utils/parse-filekey.ts
@@ -1,10 +1,5 @@
 import path from 'path';
 
 export function parseFileKey(fileKey, pad) {
-    return (
-        path.basename(fileKey, path.extname(fileKey)) +
-        pad +
-        '.' +
-        path.extname(fileKey)
-    );
+    return `${path.basename(fileKey, path.extname(fileKey))}${pad}${path.extname(fileKey)}`;
 }
